de625acf7f8e0408567b2c5212eae60fcaf8441c
Perkash Lal <parkashlal450@gmail.com>
2026-01-12 22:49:01 +0100

    i did setup of maven project and junit 5 setup

---
+++ src/test/java/it/university/advprog/EmployeeShiftSchedulerTest.java
@@ -0,0 +1,14 @@
+package it.university.advprog;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeShiftSchedulerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
