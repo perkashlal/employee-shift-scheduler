e3491749f76bb69d3729691967c37cd5cc3c8908
Perkash Lal <parkashlal450@gmail.com>
2026-01-13 23:31:28 +0100

    GREEN: addShift increments scheduled shifts

--- src/main/java/it/university/advprog/EmployeeShiftScheduler.java
+++ src/main/java/it/university/advprog/EmployeeShiftScheduler.java
@@ -2,13 +2,13 @@ package it.university.advprog;
 
 public class EmployeeShiftScheduler {
 
-    private int scheduledShifts;
-
-    public EmployeeShiftScheduler() {
-        this.scheduledShifts = 0;
-    }
+    private int scheduledShifts = 0;
 
     public int getNumberOfScheduledShifts() {
         return scheduledShifts;
+    }
+
+    public void addShift() {
+        scheduledShifts++;
     }
 }
