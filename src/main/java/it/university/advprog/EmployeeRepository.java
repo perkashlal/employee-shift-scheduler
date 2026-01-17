ad2e12bba60c13218672640c044e997a05f45c20
Perkash Lal <parkashlal450@gmail.com>
2026-01-17 01:54:20 +0100

    Introduce EmployeeRepository abstraction

---
+++ src/main/java/it/university/advprog/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package it.university.advprog;
+
+import java.util.List;
+import it.university.advprog.Employee;
+
+public interface EmployeeRepository {
+
+    List<Employee> findAll();
+
+    Employee findById(String id);
+
+    void save(Employee employee);
+
+    void delete(String id);
+}
